987d7d009bf56550ec93f8b9ce94ce7448c4370a
ash2703 <nahtnam2703@gmail.com>
2020-11-25 18:05:31 +0530

    visualize backward model

--- resnet.py
+++ resnet.py
@@ -1,89 +1,109 @@
 import torch
-import numpy as np
 import torch.nn as nn
+from torchviz import make_dot
 
 class block(nn.Module):
-    def __init__(self, in_channel, out_channel, downsample = None, stride = 1):
+    def __init__(self, in_channel, out_channel, stride = 1, padding = 0, downsample = None):
         super(block, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size = 1, stride = 1, padding = 0)
-        self.bn1 = nn.BatchNorm2d(out_channel)
-        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size = 3, stride = stride, padding = 1)
-        self.bn2 = nn.BatchNorm2d(out_channel)
-        self.conv3 = nn.Conv2d(out_channel, out_channel * 4, kernel_size = 1, stride = 1, padding = 0)
-        self.bn3 = nn.BatchNorm2d(out_channel * 4)
-
-        self.relu = nn.ReLU()
         self.downsample = downsample
-    
+        self.conv1 = nn.Conv2d(in_channel, out_channel // 4, kernel_size = 1, stride = 1, padding = 0)
+        self.bn1 = nn.BatchNorm2d(out_channel // 4)
+        self.conv2 = nn.Conv2d(out_channel // 4, out_channel // 4, kernel_size = 3, stride = stride, padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channel // 4)
+        self.conv3 = nn.Conv2d(out_channel // 4, out_channel, kernel_size = 1, stride = 1, padding = 0)
+        self.bn3 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+
     def forward(self, x):
-        identity = x
+        original = x
+        # print(x.shape)
         x = self.conv1(x)
         x = self.bn1(x)
+        x = self.relu(x)
+        # print("con1 bn", x.shape)
         x = self.conv2(x)
         x = self.bn2(x)
+        x = self.relu(x)
+        # print("con2 ", x.shape)
         x = self.conv3(x)
         x = self.bn3(x)
-
-        if self.downsample:
-            identity = self.downsample(identity)
-        print("merging")
-        x += identity
         x = self.relu(x)
+        # print("con3 ", x.shape)
+        # print(self.downsample)
+        if self.downsample:
+            # print(x.shape)
+            original = self.downsample(original)
+        #     print("downsampling ", x.shape)
+        # print("merging")
+        x += original
+        # print(x.shape)
+        x = self.relu(x)
+        print("1 block done", x.shape)
         return x
-#layer 1 stride 1
-#layer 2 3 4 stride 2  
-class resnet(nn.Module):
-    def __init__(self, block, layers, img_channel, num_classes):
-        super(resnet, self).__init__()
+    
+class ResNet(nn.Module):
+    def __init__(self, block, layers):
+        super(ResNet, self).__init__()
         self.in_channel = 64
-        self.conv1 = nn.Conv2d(img_channel, 64, kernel_size = 7, stride = 2, padding = 3)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3)
         self.bn1 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
         self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer1 = self._make_layer(block, layers[0], 64, 1)
-        self.layer2 = self._make_layer(block, layers[1], 128, 2)
-        self.layer3 = self._make_layer(block, layers[2], 256, 2)
-        self.layer4 = self._make_layer(block, layers[3], 512, 2)
+
+        self.layer1 = self._make_layer(block, 64, 1, layers[0])
+        self.layer2 = self._make_layer(block, 128, 2, layers[1])
+        self.layer3 = self._make_layer(block, 256, 2, layers[2])
+        self.layer4 = self._make_layer(block, 512, 2, layers[3])
+
         self.avgPool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc = nn.Linear(512 * 4, num_classes)
+        self.linear = nn.Linear(2048, 1000)
+        self.softmax = nn.Softmax()
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
+        x = self.pool(x)
         x = self.relu(x)
-        x = self.maxpool(x)
         x = self.layer1(x)
+        print("layer 1 shape: ", x.shape)
         x = self.layer2(x)
+        print("layer 2 shape: ", x.shape)
         x = self.layer3(x)
+        print("layer 3")
         x = self.layer4(x)
+        print("layer 4 ", x.shape)
+
         x = self.avgPool(x)
+        print(x.shape)
         x = x.reshape(x.shape[0], -1)
-        x = self.fc(x)
+        print(x.shape)
+        x = self.linear(x)
+
         return x
 
-    def _make_layer(self, block, num_blocks, out_channel, stride):
-        downsample = None
-        layers = []
-        print(num_blocks)
-        if stride != 1 or self.in_channel != out_channel * 4: 
-            downsample = nn.Sequential(nn.Conv2d(self.in_channel, out_channel * 4, kernel_size = 1, stride = stride),
-                                        nn.BatchNorm2d(out_channel * 4)
-                                        )
-        layers.append(block(self.in_channel, out_channel, downsample, stride))
-        self.in_channel = out_channel * 4
 
+        
+
+    def _make_layer(self, block, in_channel, stride, num_blocks):
+        layers = []
+        print(self.in_channel)
+        downsample = nn.Sequential(nn.Conv2d(self.in_channel, in_channel * 4, kernel_size = 1, stride = stride),
+                                   nn.BatchNorm2d(in_channel * 4))
+        
+
+        layers.append(block(self.in_channel, in_channel * 4, stride = stride, downsample = downsample))
+        
         for i in range(num_blocks - 1):
-            layers.append(block(self.in_channel, out_channel))
+            layers.append(block(in_channel * 4, in_channel * 4))
+        self.in_channel = in_channel * 4
         return nn.Sequential(*layers)
 
-def resnet50(img_channels = 3, num_classes = 1000):
-    return resnet(block, [3, 4, 6, 3], img_channels, num_classes)
-
-model = resnet50()
+resnet50 = ResNet(block, [3, 4, 6, 3])
+print(resnet50)
 
 inp = torch.randn(2, 3, 224, 224)
-print(model(inp).shape)
-
+out = resnet50(inp)
+make_dot(out).render("attached", format="png")
 
 
 
